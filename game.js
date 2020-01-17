76b860d518cece3221a1ad33895d2493cdd17d5c
janelle mellor <janellemellor@gmail.com>
2020-01-16 18:02:05 -0800

    add pair programming

--- game.js
+++ game.js
@@ -1,31 +1,30 @@
-const areYouSure = document.getElementById('are-you-sure')
-const resultDisplay = document.getElementById('result-display')
-const computerChoice = getThrowFromNumber();
-const result = checkResult(userChoice, computerChoice);
-console.log('user', userChoice);
-console.log('computer', computerChoice);
-console.log('result');
+import checkResult from '../rockPaperScissor.js';
+import getThrowFromNumber from '../game2.js';
 
 
-function getThrowFromNumber(number) {
-    if(number === 0) 'rock';
-    if(number === 1) 'paper';
-    if(number === 2) 'scissors';
-    if(number ===0) return 'rock';
-    if(number ===1) return 'paper';
-    if(number ===2) return 'scissiors';
+const areYouSure = document.getElementById('are-you-sure');
 
-}
+const resultDisplay = document.getElementById('result-display');
 
-
-
-
-
+// add the rest of your DOM elements
 
 
 areYouSure.addEventListener('click', () => {
     console.log('areyousure')
+
+    const computerChoice = getThrowFromNumber();
+    const result = checkResult(player, computer);
+
+
+    console.log('user', userChoice);
+    console.log('computer', computerChoice);
+    console.log('result');
+
+});
+
+
+    
 reset.addEventListener('click' , () => {
 
-});    
+    
 });    
